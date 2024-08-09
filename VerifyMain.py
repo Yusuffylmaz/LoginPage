976ce469942dcb667c3eb0737667f606c00106b3
Yusuf <94742414+Yusuffylmaz@users.noreply.github.com>
2024-08-09 18:10:12 +0300

    Update VerifyMain.py

--- VerifyMain.py
+++ VerifyMain.py
@@ -12,7 +12,7 @@ customtkinter.set_appearance_mode("Light")  # system (default), light, dark
 customtkinter.set_default_color_theme("blue")  # blue (default), dark-blue, green
 bg = "#ebebeb"
 wColor = "grey"
-#StartTimedUpdates() #kişiye özel olan 'code' değerini 30 saniyede bir veritabanında günceller
+#StartTimedUpdates() #Kişiye özel olan 'code' değerini 30 saniyede bir günceller. Sadece 7/24 açık olacak olan sayfada bu kodun aktif olması gerekiyor.
 
 def ValidateMail(mail):
     pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
